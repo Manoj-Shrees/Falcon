2aca8c370cc89a5ab9191735d5a5d2956adc9f84
Manoj Shrees <manojshrees77@gmail.com>
2024-11-19 17:08:25 +1100

    Initial Commit

---
+++ Falcon/FalconApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconApp.swift
+//  Falcon
+//
+//  Created by Manoj Shrees on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FalconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FalconTests/FalconTests.swift
@@ -0,0 +1,17 @@
+//
+//  FalconTests.swift
+//  FalconTests
+//
+//  Created by Manoj Shrees on 19/11/2024.
+//
+
+import Testing
+@testable import Falcon
+
+struct FalconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
